541b5630ce68ac616dc5b6db215546f101e73bfa
aisven <aisven822@gmail.com>
2023-04-26 00:23:23 +0200

    added pytest example

--- src/predict.py
+++ src/predict.py
@@ -2,12 +2,19 @@ import numpy as np
 import pickle
 
 
+def predict(regressor, n_features, X_np):
+    y_pred_np = regressor.predict(X_np.reshape(-1, n_features))
+    return y_pred_np
+
+
 def main():
     regressor = pickle.load(open("target/models/Miles_per_Gallon_Regressor.pickle", "rb"))
 
-    y_pred = regressor.predict(np.asarray([6.0, 200.0, 2500.0, 9.5, 99.0]).reshape(-1, 5))
+    X_np = np.asarray([6.0, 200.0, 2500.0, 9.5, 99.0])
 
-    print(y_pred)
+    y_pred_np = regressor.predict(X_np.reshape(-1, 5))
+
+    print(y_pred_np)
 
 
 if __name__ == "__main__":
