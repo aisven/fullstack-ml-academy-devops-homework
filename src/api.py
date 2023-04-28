dafe9772096a0df9519fb29cd2db7504bd644bbd
aisven <aisven822@gmail.com>
2023-04-28 21:00:00 +0200

    add flask training data GET route

--- src/api.py
+++ src/api.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+import pandas as pd
+from werkzeug import Response
 
 app = Flask(__name__)
 
@@ -9,3 +11,10 @@ CORS(app)
 @app.route("/hello")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/training_data")
+def get_training_data():
+    # this is a super naive implementation just to try out Flask for a bit
+    dataset_pd = pd.read_csv("data/auto-mpg.csv", sep=";", skipinitialspace=True)
+    return Response(dataset_pd.to_json(orient="records"), mimetype="application/json")
