9bce55acece832cecf926b61b894ec8cf353781a
aisven <aisven822@gmail.com>
2023-04-28 21:06:29 +0200

    add flask root GET route

--- src/api_requests.py
+++ src/api_requests.py
@@ -1,17 +1,17 @@
 import requests
 
 
-def get_hello() -> requests.Response:
-    url_hello_local = "http://127.0.0.1:5000/hello"
-    res = requests.get(url_hello_local)
+def get_hello_world() -> requests.Response:
+    url_hello_world_local = "http://127.0.0.1:5000/hello_world"
+    res = requests.get(url_hello_world_local)
     return res
 
 
 def main():
-    res_hello: requests.Response = get_hello()
-    res_hello_str = str(res_hello.content.decode(res_hello.encoding))
-    print(res_hello.status_code)
-    print(res_hello_str)
+    res_hello_world: requests.Response = get_hello_world()
+    res_hello_world_str = str(res_hello_world.content.decode(res_hello_world.encoding))
+    print(res_hello_world.status_code)
+    print(res_hello_world_str)
 
 
 if __name__ == "__main__":
