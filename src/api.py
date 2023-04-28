9cc1d039a0bb3d8d644ab1e88f3063a6cbe12a65
aisven <aisven822@gmail.com>
2023-04-28 21:42:20 +0200

    add flask predict GET route

--- src/api.py
+++ src/api.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import json
+from flask import request
 from flask_cors import CORS
+import numpy as np
 import pandas as pd
+import pickle
 from werkzeug import Response
 
 app = Flask(__name__)
@@ -23,3 +27,29 @@ def get_training_data():
     # this is a super naive implementation just to try out Flask for a bit
     dataset_pd = pd.read_csv("data/auto-mpg.csv", sep=";", skipinitialspace=True)
     return Response(dataset_pd.to_json(orient="records"), mimetype="application/json")
+
+
+@app.route("/predict")
+def get_predict():
+    # this is a super naive implementation just to try out Flask for a bit
+    zylinder = request.args.get("zylinder", type=float)
+    ps = request.args.get("ps", type=float)
+    gewicht = request.args.get("gewicht", type=float)
+    beschleunigung = request.args.get("beschleunigung", type=float)
+    baujahr = request.args.get("baujahr", type=float)
+
+    X_np = np.asarray([zylinder, ps, gewicht, beschleunigung, baujahr])
+
+    regressor = pickle.load(open("target/models/Miles_per_Gallon_Regressor.pickle", "rb"))
+
+    y_pred_np = regressor.predict(X_np.reshape(-1, 5))
+
+    y_pred = y_pred_np.item()
+
+    y_pred_dict = {"result": y_pred}
+
+    res = app.response_class(
+        response=json.dumps(y_pred_dict), status=200, mimetype="application/json"
+    )
+
+    return res
--- src/api_requests.py
+++ src/api_requests.py
@@ -7,11 +7,22 @@ def get_hello_world() -> requests.Response:
     return res
 
 
+def get_predict() -> requests.Response:
+    url_predict_local = "http://127.0.0.1:5000/predict?zylinder=6&ps=133&gewicht=3410&beschleunigung=15.8&baujahr=78"
+    res = requests.get(url_predict_local)
+    return res
+
+
 def main():
     res_hello_world: requests.Response = get_hello_world()
-    res_hello_world_str = str(res_hello_world.content.decode(res_hello_world.encoding))
     print(res_hello_world.status_code)
+    res_hello_world_str = str(res_hello_world.content.decode(res_hello_world.encoding))
     print(res_hello_world_str)
+
+    res_predict: requests.Response = get_predict()
+    print(res_predict.status_code)
+    res_predict_str = str(res_predict.content.decode(res_predict.encoding))
+    print(res_predict_str)
 
 
 if __name__ == "__main__":
